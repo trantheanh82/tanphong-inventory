55562c97cfccb1d056e53666a73911ef958a18a6
The Anh Tran <raijuichi@gmail.com>
2025-08-22 05:56:22 +0000

    this is a pwa app, and disable zoom out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { AppShell } from '@/components/app-shell';
@@ -21,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#111827" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className="font-body antialiased">
         <div className="absolute inset-0 z-0 animated-gradient-bg"></div>
