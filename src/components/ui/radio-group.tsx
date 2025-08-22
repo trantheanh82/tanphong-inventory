6792303ed8050e2c963fab9e6c63dc76f43544ae
The Anh Tran <raijuichi@gmail.com>
2025-08-22 07:19:42 +0000

    make radio button is black/white

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -28,7 +28,8 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square h-5 w-5 rounded-full border border-gray-800 text-gray-800 ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "data-[state=checked]:bg-gray-800 data-[state=checked]:text-white",
         className
       )}
       {...props}
